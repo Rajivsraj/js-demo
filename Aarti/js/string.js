baa522ebe068433112f42ce9b55a738e62958c2a
Rajiv <rajivsraj78@gmail.com>
2023-08-24 12:59:04 +0530

    string done

--- Aarti/js/string.js
+++ Aarti/js/string.js
@@ -41,8 +41,50 @@ console.log(str);
 document.write(str);
 */
 
-
+/*
 let names = "Rahul";
 let age = 23;
 
 console.log(`My name is ${names} and my age is ${age} ${5+4}`);
+*/
+
+// let nm = "Rahul";
+// console.log(nm.length);
+// console.log(nm.charAt(2));  // value behalf of index no
+
+// will tell you ascii code on indexed value
+// console.log(nm.charCodeAt(1))
+// let lnm = "Kumar"
+
+// let fullname = nm.concat(" ",lnm);
+// console.log(fullname);
+
+
+// let str = "hello";
+// console.log(str.endsWith("s"));
+// console.log(str.lastIndexOf('o'))
+
+// let str = "Hello hoW - Are how - you     ";
+// n = str.replace("how","is");
+// n = str.replaceAll("how","is");
+// n = str.search("how");  // will return the index no of string
+
+// n = str.slice(1, 4);
+// n = str.split("-");
+// n = str.substring(1,4)
+// n = str.toLowerCase()
+// n = str.toUpperCase()
+
+// no = 5;
+// no = no.toString()
+// console.log(typeof no);
+
+
+// let str = "Hello hoW - Are how - you     ";
+// str = str.trimEnd();
+// console.log(str);
+
+str = "hello";
+
+x = str.at(2);
+console.log(x);
