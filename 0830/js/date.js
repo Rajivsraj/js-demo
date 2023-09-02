714fbb1ae59aec7bb50fe41c6f55c750021d9636
Rajiv <rajivsraj78@gmail.com>
2023-09-02 09:19:41 +0530

    by id and tagname

---
+++ 0830/js/date.js
@@ -0,0 +1,21 @@
+// let dt = new Date()
+
+
+// let dt = new Date(2024, 1, 28, 12, 33, 55)
+
+// let dt = new Date(2024, 1, 28)
+// let dt = new Date("2024, 1, 28")
+let dt = new Date("2024 1 28")
+
+y = dt.getFullYear()
+m = dt.getMonth()
+
+x = dt.setFullYear(2030)
+
+cy = dt.getFullYear()
+
+console.log(dt);
+console.log(y);
+console.log(m);
+console.log(cy);
+
---
+++ 0830/js/maths.js
@@ -0,0 +1,44 @@
+x = Math.PI;
+
+x = Math.abs(-20);
+
+x = Math.trunc(345345.4556)
+
+x = Math.sqrt(16)
+
+x = Math.min(54,656,7,56,7,545,34,5,56)
+
+x = Math.max(54,656,7,56,7,545,34,5,56)
+
+x = Math.floor(45.65)
+
+x = Math.ceil(45.1)
+
+x = Math.round(45.45)
+x = Math.round(45.51)
+
+x = Math.trunc(Math.random()*100)
+
+// document.write(x)
+
+x = Number.isInteger(parseInt("20"))
+
+x = Number.isSafeInteger(997564564561651)
+x = Number.MAX_SAFE_INTEGER
+// x = Number.MIN_SAFE_INTEGER
+x = Number.isSafeInteger(x)
+x = Number.MAX_VALUE
+
+// x = 40;
+// x = x.toExponential(2)
+// 4.e+1
+// 4*(1)10 = 50
+
+// x = 23;
+// x = x.toPrecision(5)
+
+x = 345546;
+// x = x.toString(2)
+x = x.toString(16) // 2, 8, 10, 16
+
+console.log(x);
