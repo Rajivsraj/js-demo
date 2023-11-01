87130c0db0e8065727cd8d41d2313b31d1a1d5d4
Rajiv <rajivsraj78@gmail.com>
2023-11-01 19:23:13 +0530

    export import

---
+++ Aarti/dom/modules/app.js
@@ -0,0 +1,40 @@
+// Named export
+// var a = 10;
+
+// function add(a, b){
+//     console.log(a+b)
+// }
+
+// class Student{
+//     constructor(){
+//         this.city = "delhi";
+//         this.cls = "BA"
+//     }
+//     age(){
+//         return 20;
+//     }
+// }
+
+
+// variable export
+// export {a};
+// export {add}
+// export {Student}
+
+
+// export at once
+// export {a, add, Student}
+
+
+
+
+// ==============
+// default export
+
+function add(a, b){
+    console.log(a+b)
+}
+
+var x = 20;
+
+export default add;
---
+++ Aarti/dom/modules/module.js
@@ -0,0 +1,31 @@
+// named import
+// import {a} from "./app.js"
+// import {add} from "./app.js"
+// import { Student } from "./app.js";
+
+// import at once
+// import {a, add, Student} from "./app.js";
+
+// console.log(a);
+// add(10, 20);
+
+// var obj = new Student();
+// console.log(obj.age());
+
+
+// import all
+// import {Student as stu, add, a} from "./app.js";
+// import {Student as stu, add as ad, a} from "./app.js";
+
+// var obj = new stu()
+
+// console.log(obj.city)
+
+// console.log(a)
+// add(1450, 4)
+
+// ad(10, 50)
+
+import add from "./app.js"
+
+add(10, 20)
